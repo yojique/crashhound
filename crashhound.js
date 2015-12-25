2d7db66b46bac1a200f79b49482a25c084a4f325
Andrii Shpak <npm@molo.co.ua>
2015-12-25 17:02:35 +0200

    arguments

--- crashhound.js
+++ crashhound.js
@@ -1,3 +1,45 @@
-#! /usr/bin/env node
+#! /usr/bin/env node --harmony
 
-console.log("crashhound");
+"use strict";
+
+let _         = require('underscore');
+let assert    = require('assert');
+let co        = require('co');
+let commander = require('commander');
+
+let CrashLog = require('./lib/CrashLog');
+
+
+let crashlog;
+
+commander
+  .version('1.0.0')
+  .usage('[options] <crashlog>')
+  .arguments('<crashlog>')
+  .action(function (crashlog_path) {
+    crashlog = new CrashLog(crashlog_path);
+  })
+  .parse(process.argv);
+
+if (_.isEmpty(crashlog)) {
+  commander.help();
+}
+
+co(function * () {
+
+  try {
+    assert(crashlog.originalResolve(),          "Couldn't find crash log file.");
+    assert(crashlog.originalSizeLimit(),        "File specified is to large to be crash log.");
+    assert(yield crashlog.originalReadLines(),  "Couldn't read crash log file.");
+  } catch (e) {
+    if (_.has(e, 'message')) {
+      e = e.message;
+    }
+
+    console.log(e);
+  }
+
+  console.log(crashlog);
+
+});
+
